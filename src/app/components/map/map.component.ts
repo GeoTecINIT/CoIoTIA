2dbe5a991c73598788810d2e0085f52ff92eb48e
angrui14 <angruifas.13@gmail.com>
2025-03-14 13:23:18 +0100

    Added map layers

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -55,14 +55,29 @@ export class MapComponent implements AfterViewInit {
     });
 
     const topographicLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
+      maxZoom: 20,
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
+    const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 20,
+      minZoom: 3,
+      attribution: '&copy; <a href="https://www.esri.com/en-us/arcgis/products/arcgis-online/overview">Esri</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+
+    const esriWorldLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 20,
+      minZoom: 3,
+      attribution: '&copy; <a href="https://www.esri.com/en-us/arcgis/products/arcgis-online/overview">Esri</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+
+    
      const maps = {
       "OpenStreetMap" : openStreetLayer,
       "Topographic" : topographicLayer,
+      "Satellite" : satelliteLayer,
+      "Esri" : esriWorldLayer,
     }
 
     openStreetLayer.addTo(this.map);
